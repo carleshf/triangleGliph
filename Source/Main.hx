32bdfd6fd08ad4fb8f60c11a656cc108388efe8a
chernandez <chernandez@crunch>
2020-02-13 17:12:35 +0100

    Major reorder.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,11 +1,8 @@
 package;
 
-//import haxe.Constraints.Function;
-import haxe.macro.Compiler.IncludePosition;
 import openfl.display.Sprite;
 import openfl.display.Shape;
 import openfl.events.MouseEvent;
-//import openfl.text.Font;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.Assets;
@@ -29,41 +26,137 @@ class TriangleGliph	{
 		_nelm = 1;
 	}
 
-	public function addPieceByNum(num:Int) {
-		if(num >= 0 && num <= 11) {
-			_content.push(num);
-		}
-	}
-
-	public function setGrid(status) {
-		_grid = status;
-	}
-
-	public function setColor(color) {
-		_color = color;
-	}
-
-	public function setAlpha(alpha) {
-		_alpha = alpha;
-	}
-
-	private function drawGrid(canvas, offsetX, offsetY, gap, size) {
-		var drawer = new Shape ();
-		drawer.graphics.beginFill(0x000000, 0.25);
-
-		for(ii in 1...6) {
-			var x = offsetX + (gap * ii);
-			for(jj in 1...6) {
-				var y = offsetY + (gap * jj);
-				drawer.graphics.drawRect(
-					x, y, size, size
-				);
+	public function generate() {
+		/*	Creation of a Triangular Glyph
+			------------------------------
+			1. First decide typo of border: 
+				* 0.9 for closed ....... 0
+				* 0.1 for opened ....... 1
+			2. Decide filling elements:
+				* 0.6 for one element 
+				* 0.3 for two elements
+				* 0.1 for three elements
+			Using as elements (all equi-probables):
+				* for ceiling .........  2
+				* for belt ............  3
+				* lance ...............  4
+				* for sad .............  5
+				* for happy ...........  6
+				* for large sad .......  7
+				* for large happy .....  8
+				* for leg .............  9
+				* for down diag ....... 10
+				* for up diag ......... 11
+		*/
+		function updateToAddArray(addedValue:Int, elmsToAdd:Array<Int>) {
+			function remove(fullArray:Array<Int>, elmsRemove:Array<Int>) {
+				for(elm in elmsRemove) {
+					fullArray = fullArray.filter(function(item) return item != elm);
+				}
+				return fullArray;
 			}
+			// Filter addValue
+			var newElmsToAdd = remove(elmsToAdd, [addedValue]);
+			
+			// Added diagonals --> remove happy/sad , leg , large sad , diagonals
+			if(addedValue == 10 || addedValue == 11) {
+				newElmsToAdd = remove(elmsToAdd, [5, 6, 9, 10, 11]);
+			}
+			// Added leg --> remove diagonals
+			if(addedValue == 9) {
+				newElmsToAdd = remove(elmsToAdd, [10, 11]);
+			}
+			// Added large sad --> remove diagonals
+			if(addedValue == 7) {
+				newElmsToAdd = remove(elmsToAdd, [10, 11]);
+			}
+			// Added happy --> remove diagonals and large happy
+			if(addedValue == 6 || addedValue == 8) {
+				newElmsToAdd = remove(elmsToAdd, [6, 8, 10, 11]);
+			}
+			// Added sad --> remove diagonals and large sad
+			if(addedValue == 5 || addedValue == 7) {
+				newElmsToAdd = remove(elmsToAdd, [5, 7, 10, 11]);
+			}
+			// Added belt/lance --> remove lance/belt
+			if(addedValue == 3 || addedValue == 4) {
+				newElmsToAdd = remove(elmsToAdd, [3, 4]);
+			}
+			// Added ceeling/lance --> remove lance/ceeling
+			if(addedValue == 2 || addedValue == 4) {
+				newElmsToAdd = remove(elmsToAdd, [2, 4]);
+			}
+
+			return newElmsToAdd;
 		}
 
-		canvas.addChild(drawer);
+		// 1. Select border
+		if(Math.random() <= 0.1) {
+			_content.push(1);
+		} else {
+			_content.push(0);
+		}
+
+		// 2. Select number of elements to add
+		var pelm = Math.random();
+		if (pelm <= 0.1) {
+			_nelm = 3;
+		} else if(pelm <= 0.3) {
+			_nelm = 2;
+		}
+
+		// 3. Fill the glyph
+		var elmsToAdd = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+		for(ii in 0..._nelm) {
+			var val = Std.random(elmsToAdd.length - 1);
+			_content.push(elmsToAdd[val]);
+			elmsToAdd = updateToAddArray(elmsToAdd[val], elmsToAdd);
+		}
 	}
-	
+
+	public function clean(canvas:Sprite) {
+		for(part in _shapes) {
+			canvas.removeChild(part);
+		}
+		_shapes = [];
+	}
+
+	public function draw(canvas:Sprite, oX:Int, oY:Int, gap:Int, stroke:Int) {
+		if(_grid) {
+			drawGrid(canvas, oX, oY, gap, stroke);
+		}
+		for(elm in _content) {
+			var part:Shape = null;
+			switch (elm) {
+				case 0:
+					part = drawCloseTriangle(canvas, oX, oY, gap, stroke);
+				case 1:
+					part = drawOpenTriangle(canvas, oX, oY, gap, stroke);
+				case 2:
+					part = drawCeilingLine(canvas, oX, oY, gap, stroke);
+				case 3:
+					part = drawBeltLine(canvas, oX, oY, gap, stroke);
+				case 4:
+					part = drawLanceLine(canvas, oX, oY, gap, stroke);
+				case 5:
+					part = drawSadLine(canvas, oX, oY, gap, stroke);
+				case 6:
+					part = drawHappyLine(canvas, oX, oY, gap, stroke);
+				case 7:
+					part = drawSadLargeLine(canvas, oX, oY, gap, stroke);
+				case 8:
+					part = drawHappyLargeLine(canvas, oX, oY, gap, stroke);
+				case 9:
+					part = drawLegLine(canvas, oX, oY, gap, stroke);
+				case 10:
+					part = drawDiagonalDownLine(canvas, oX, oY, gap, stroke);
+				case 11:
+					part = drawDiagonalUpLine(canvas, oX, oY, gap, stroke);
+			}
+			_shapes.push(part);
+		}
+	}
+
 	private function drawCloseTriangle(canvas:Sprite, oX:Int, oY:Int, gap:Int, stroke:Int):Shape {
 		var triangle = new Shape ();
 		triangle.graphics.lineStyle (stroke, _color, _alpha);
@@ -179,137 +272,43 @@ class TriangleGliph	{
 		return triangle;
 	}
 
-	public function clean(canvas:Sprite) {
-		for(part in _shapes) {
-			canvas.removeChild(part);
+	private function drawGrid(canvas, offsetX, offsetY, gap, size) {
+		var drawer = new Shape ();
+		drawer.graphics.beginFill(0x000000, 0.25);
+
+		for(ii in 1...6) {
+			var x = offsetX + (gap * ii);
+			for(jj in 1...6) {
+				var y = offsetY + (gap * jj);
+				drawer.graphics.drawRect(
+					x, y, size, size
+				);
+			}
 		}
-		_shapes = [];
+
+		canvas.addChild(drawer);
+		_shapes.push(drawer);
 	}
 
-	public function draw(canvas:Sprite, oX:Int, oY:Int, gap:Int, stroke:Int) {
-		if(_grid) {
-			drawGrid(canvas, oX, oY, gap, stroke);
-		}
-		for(elm in _content) {
-			var part:Shape = null;
-			switch (elm) {
-				case 0:
-					part = drawCloseTriangle(canvas, oX, oY, gap, stroke);
-				case 1:
-					part = drawOpenTriangle(canvas, oX, oY, gap, stroke);
-				case 2:
-					part = drawCeilingLine(canvas, oX, oY, gap, stroke);
-				case 3:
-					part = drawBeltLine(canvas, oX, oY, gap, stroke);
-				case 4:
-					part = drawLanceLine(canvas, oX, oY, gap, stroke);
-				case 5:
-					part = drawSadLine(canvas, oX, oY, gap, stroke);
-				case 6:
-					part = drawHappyLine(canvas, oX, oY, gap, stroke);
-				case 7:
-					part = drawSadLargeLine(canvas, oX, oY, gap, stroke);
-				case 8:
-					part = drawHappyLargeLine(canvas, oX, oY, gap, stroke);
-				case 9:
-					part = drawLegLine(canvas, oX, oY, gap, stroke);
-				case 10:
-					part = drawDiagonalDownLine(canvas, oX, oY, gap, stroke);
-				case 11:
-					part = drawDiagonalUpLine(canvas, oX, oY, gap, stroke);
-			}
-			_shapes.push(part);
+	public function addPieceByNum(num:Int) {
+		if(num >= 0 && num <= 11) {
+			_content.push(num);
 		}
 	}
 
-	public function generate() {
-		/*	Creation of a Triangular Glyph
-			------------------------------
-			1. First decide typo of border: 
-				* 0.9 for closed ....... 0
-				* 0.1 for opened ....... 1
-			2. Decide filling elements:
-				* 0.6 for one element 
-				* 0.3 for two elements
-				* 0.1 for three elements
-			Using as elements (all equi-probables):
-				* for ceiling .........  2
-				* for belt ............  3
-				* lance ...............  4
-				* for sad .............  5
-				* for happy ...........  6
-				* for large sad .......  7
-				* for large happy .....  8
-				* for leg .............  9
-				* for down diag ....... 10
-				* for up diag ......... 11
-		*/
-		function updateToAddArray(addedValue:Int, elmsToAdd:Array<Int>) {
-			function remove(fullArray:Array<Int>, elmsRemove:Array<Int>) {
-				for(elm in elmsRemove) {
-					fullArray = fullArray.filter(function(item) return item != elm);
-				}
-				return fullArray;
-			}
-			// Filter addValue
-			var newElmsToAdd = remove(elmsToAdd, [addedValue]);
-			
-			// Added diagonals --> remove happy/sad , leg , large sad , diagonals
-			if(addedValue == 10 || addedValue == 11) {
-				newElmsToAdd = remove(elmsToAdd, [5, 6, 9, 10, 11]);
-			}
-			// Added leg --> remove diagonals
-			if(addedValue == 9) {
-				newElmsToAdd = remove(elmsToAdd, [10, 11]);
-			}
-			// Added large sad --> remove diagonals
-			if(addedValue == 7) {
-				newElmsToAdd = remove(elmsToAdd, [10, 11]);
-			}
-			// Added happy --> remove diagonals and large happy
-			if(addedValue == 6 || addedValue == 8) {
-				newElmsToAdd = remove(elmsToAdd, [6, 8, 10, 11]);
-			}
-			// Added sad --> remove diagonals and large sad
-			if(addedValue == 5 || addedValue == 7) {
-				newElmsToAdd = remove(elmsToAdd, [5, 7, 10, 11]);
-			}
-			// Added belt/lance --> remove lance/belt
-			if(addedValue == 3 || addedValue == 4) {
-				newElmsToAdd = remove(elmsToAdd, [3, 4]);
-			}
-			// Added ceeling/lance --> remove lance/ceeling
-			if(addedValue == 2 || addedValue == 4) {
-				newElmsToAdd = remove(elmsToAdd, [2, 4]);
-			}
+	public function setGrid(status) {
+		_grid = status;
+	}
 
-			return newElmsToAdd;
-		}
+	public function setColor(color) {
+		_color = color;
+	}
 
-		// 1. Select border
-		if(Math.random() <= 0.1) {
-			_content.push(1);
-		} else {
-			_content.push(0);
-		}
-
-		// 2. Select number of elements to add
-		var pelm = Math.random();
-		if (pelm <= 0.1) {
-			_nelm = 3;
-		} else if(pelm <= 0.3) {
-			_nelm = 2;
-		}
-
-		// 3. Fill the glyph
-		var elmsToAdd = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-		for(ii in 0..._nelm) {
-			var val = Std.random(elmsToAdd.length - 1);
-			_content.push(elmsToAdd[val]);
-			elmsToAdd = updateToAddArray(elmsToAdd[val], elmsToAdd);
-		}
+	public function setAlpha(alpha) {
+		_alpha = alpha;
 	}
 }
+
 
 class Button extends Sprite {
 	private var _text:TextField;
@@ -318,10 +317,12 @@ class Button extends Sprite {
 		super();
 		this.mouseChildren = false;
 		this.buttonMode = true;
-		// Get the new font
-		var textFieldFormat:TextFormat = new TextFormat(Assets.getFont("fonts/DANUBE__.TTF").fontName, 17 , 0x000000);
+
+		this.graphics.beginFill(0xffffff);
+		this.graphics.drawRect(px, py, 40, 40);
+		this.graphics.endFill();
+
 		_text = new TextField();
-		_text.defaultTextFormat = textFieldFormat;
 		_text.x = px;
 		_text.y = py;
 		_text.width = w;
@@ -329,27 +330,50 @@ class Button extends Sprite {
 		_text.text = text;
 		_text.background = true;
 		_text.backgroundColor = 0x991155;
-		parent.addChild(_text);
+		this.addChild(_text);
 	}
-
-	// public function addCallback(handler) {
-	// 	trace(_text.text);
-	// 	this.addEventListener(MouseEvent.CLICK, handler);
-	// }
 }
 
+
 class Main extends Sprite {
-	static public function clearCollection(canvas:Sprite, collection:Array<TriangleGliph>) {
-		for(glyph in collection) {
-			glyph.clean(canvas);
+	private var _btnWall:Button;
+	private var _btnPices:Button;
+	private var _btnAlph:Button;
+	private var _content:Array<TriangleGliph>;
+
+	private var _wall_x = 10;
+	private var _wall_y = 10;
+	
+	public function new () {
+		super ();
+	
+		init();
+	}
+	
+	public function init() {
+		_content = [];
+		_btnWall = new Button(this, 70, 250, 175, "Random Wall");
+		_btnPices = new Button(this, 245, 250, 75, "Pieces");
+		_btnAlph = new Button(this, 320, 250, 115, "Alphabet");
+		_btnWall.addEventListener(MouseEvent.CLICK, clickWall);
+		_btnPices.addEventListener(MouseEvent.CLICK, clickPieces);
+		_btnAlph.addEventListener(MouseEvent.CLICK, click_b2);
+		addChild(_btnWall);
+		addChild(_btnPices);
+		addChild(_btnAlph);
+	}
+
+	public function clearCollection() {
+		for(glyph in _content) {
+			glyph.clean(this);
 		}
 	}
 
-	static public function drawPieces(canvas:Sprite, oX:Int, oY:Int, glyphGap:Int, gridGap:Int, strokeSize:Int):Array<TriangleGliph> {
-		var collection:Array<TriangleGliph> = [];
+	public function drawPieces(oX:Int, oY:Int, glyphGap:Int, gridGap:Int, strokeSize:Int, nCols:Int) {
+		clearCollection();
 		for(elm in 0...12) {
 			var glyph = new TriangleGliph();
-			//glyph.setGrid(true);
+			glyph.setGrid(true);
 			glyph.setAlpha(0.75);
 			if(elm == 0 || elm == 1) {
 				glyph.setColor(0xDD8888);
@@ -357,27 +381,25 @@ class Main extends Sprite {
 				glyph.setColor(0x00AAAA);
 			}
 			glyph.addPieceByNum(elm);
-			collection.push(glyph);
+			_content.push(glyph);
 		}
 
 		var ii = 0;
 		var jj = 0;
-		for(glyph in collection) {
+		for(glyph in _content) {
 			var px = oX + ii * glyphGap;
 			var py = oY + jj * glyphGap;
-			glyph.draw(canvas, px, py, gridGap, strokeSize);
+			glyph.draw(this, px, py, gridGap, strokeSize);
 			ii = ii + 1;
-			if(ii > 3) {
+			if(ii > nCols) {
 				ii = 0;
 				jj = jj + 1;
 			}
 		}
-
-		return collection;
 	}
 
-	static public function drawRandom(canvas:Sprite, oX:Int, oY:Int, glyphGap:Int, gridGap:Int, strokeSize:Int, nRows:Int, nCols:Int):Array<TriangleGliph> {
-		var collection:Array<TriangleGliph> = [];
+	public function drawRandom(oX:Int, oY:Int, glyphGap:Int, gridGap:Int, strokeSize:Int, nRows:Int, nCols:Int) {
+		clearCollection();
 		for(ii in 0...nRows) {
 			var py = oY + ii * glyphGap;
 			for(jj in 0...nCols) {
@@ -387,59 +409,24 @@ class Main extends Sprite {
 				glyph.setColor(0x222222);
 				glyph.setAlpha(1);
 				glyph.generate();
-				glyph.draw(canvas, px, py, gridGap, strokeSize);
-				collection.push(glyph);
+				glyph.draw(this, px, py, gridGap, strokeSize);
+				_content.push(glyph);
 			}
 		}
-		return collection;
+	}
+	
+	public function clickWall(e:MouseEvent) {
+		trace("click random wall");
+		drawRandom(_wall_x, _wall_y, 50, 6, 2, 5, 10);
 	}
 
-
-
-	public function new() {
-		super();
-		this.mouseChildren = false;
-    	this.buttonMode = true;
-		init();
+	public function clickPieces(e:MouseEvent) {
+		trace("click pieces");
+		drawPieces(_wall_x, _wall_y, 100, 10, 5, 5);
 	}
 
-	public function init() {
-		var wall_x = 10;
-		var wall_y = 10;
-
-		//drawPieces(this, 10, 10, 50, 6, 2);
-
-		var randomWall = drawRandom(this, wall_x, wall_y, 50, 6, 2, 5, 10);
-		var piecesWall = [];
-		var alphabetWall = [];
-		function callbackDrawRandom(e:MouseEvent) {
-			trace("btnWall");
-			clearCollection(this, randomWall);
-			clearCollection(this, piecesWall);
-			clearCollection(this, alphabetWall);
-			randomWall = drawRandom(this, wall_x, wall_y, 50, 6, 2, 5, 10);
-		}
-		function callbackDrawPieces(e:MouseEvent) {
-			trace("btnPices");
-			clearCollection(this, randomWall);
-			clearCollection(this, piecesWall);
-			clearCollection(this, alphabetWall);
-			piecesWall = drawPieces(this, wall_x, wall_y, 50, 6, 2);
-		}
-		function callbackDrawAlphabet(e:MouseEvent) {
-			clearCollection(this, randomWall);
-			clearCollection(this, piecesWall);
-			clearCollection(this, alphabetWall);
-		}
-		
-
-		var btnWall = new Button(this, 70, 250, 175, "Random Wall");
-		var btnPices = new Button(this, 245, 250, 75, "Pieces");
-		var btnAlph = new Button(this, 320, 250, 115, "Alphabet");
-		btnWall.addEventListener(MouseEvent.CLICK, callbackDrawRandom);
-		btnPices.addEventListener(MouseEvent.CLICK, callbackDrawPieces);
-
-		
-
+	public function click_b2(e:MouseEvent) {
+		trace("test B2");
 	}
+	
 }
